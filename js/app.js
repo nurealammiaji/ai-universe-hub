4a271834e3fa390d6eaa1fad051619df94a14c7b
nurealammiaji <nurealam.miaji@gmail.com>
2023-06-08 22:40:26 +0600

    card creation by js added

--- js/app.js
+++ js/app.js
@@ -5,8 +5,25 @@ let aiDataLoad = () => {
 }
 
 let aiDataDisplay = (infos) => {
+    let aiCardDiv = document.getElementById("ai-card-div");
     infos.map(info => {
         console.log(info);
+        let aiCard = document.createElement("div");
+        aiCard.classList.add("col");
+        aiCard.innerHTML = `
+        <div class="card h-100">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-body-secondary">Last updated 3 mins ago</small>
+            </div>
+        </div>
+        `;
+        aiCardDiv.appendChild(aiCard);
+
     })
 }
 
