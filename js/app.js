f3eab6f5faa849b2aafe17857e48f4c551a74c46
nurealammiaji <nurealam.miaji@gmail.com>
2023-06-08 23:09:00 +0600

    dynamic features list added

--- js/app.js
+++ js/app.js
@@ -12,14 +12,19 @@ let aiDataDisplay = (infos) => {
         aiCard.classList.add("col");
         aiCard.innerHTML = `
         <div class="card h-100 container">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="${info.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${info.name}</h5>
-                <p class="card-text">${info.description}</p>
+                <p id="features" class="card-text">Features:</p>
+                <ol>
+                    <li>${info.features[0]}</li>
+                    <li>${info.features[1]}</li>
+                    <li>${info.features[2]}</li>
+                </ol>
             </div>
             <hr/>
             <div class="card-footer">
-                <small class="text-body-secondary">Last updated 3 mins ago</small>
+                <small class="text-body-secondary">${info.published_in}</small>
             </div>
         </div>
         `;
